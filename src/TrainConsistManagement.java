55d1e0e89c4e965ebdbf8f207be6b7450a050d92
Madhuri-Annapureddy <Madhuri-Annapureddy>
2026-03-25 10:57:52 +0530

    UC6: Map Bogie to Capacity

--- src/TrainConsistManagement.java
+++ src/TrainConsistManagement.java
@@ -1,19 +1,25 @@
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TrainConsistManagement {
 
     public static void main(String[] args){
 
-        System.out.println("=========================================");
-        System.out.println("UC5: Preserve Insertion Order of Bogies");
-        System.out.println("=========================================");
-        Set<String> formation = new LinkedHashSet<>();
-        formation.add("Engine");
-        formation.add("Sleeper");
-        formation.add("Cargo");
-        formation.add("Guard");
-        System.out.println("Final Train Formation:");
-        System.out.println(formation);
+        System.out.println("=============================");
+        System.out.println("UC6 - Map Bogie to Capacity");
+        System.out.println("=============================");
+        Map<String, Integer> capacityMap=new HashMap<>();
+
+        capacityMap.put("Sleeper", 72);
+        capacityMap.put("AC Chair", 78);
+        capacityMap.put("First Class", 24);
+        capacityMap.put("Cargo", 100);
+
+        System.out.println("Bogie Capacity Details:\n");
+
+        for (Map.Entry<String, Integer> entry : capacityMap.entrySet()) {
+            System.out.println( entry.getKey() + "->" + entry.getValue());
+        }
+
     }
 }
